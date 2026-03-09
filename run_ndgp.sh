c932a9f128a9125e427e3fd7b0f65cd1bbcd79f9
Vicente Pedreros Aballay <71951212+vPedreros@users.noreply.github.com>
2026-03-09 14:58:56 -0300

    scripts for running in COSMA

---
+++ run_ndgp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#SBATCH -p cosma7 
+#SBATCH -A dp203
+#SBATCH -t 2:00:00
+#SBATCH --ntasks 8
+
+BASE="/cosma8/data/dp203/bl267/Data/MG_Arepo_runs/FORGE-BRIDGE/BRIDGE/Particle_Snapshots/L500_N1024_Seed_4257_Node_006_Omega_m_0.35339_S8_0.78021_h_0.78052_H0rc_0.5646738_sigma8_0.71886/"
+OUTROOT="/cosma7/data/dp203/dc-pedr3/gravitomagnetic/output/ndgp/seed_4257"
+
+for snap in $(seq 24 27); do
+    out_dir="${OUTROOT}/snap_$(printf '%03d' "$snap")"
+  
+    python3 gravitomagnetic/python/read_snap.py --base-path "$BASE" --out-dir "$out_dir" --snap-num $snap 
+    python3 gravitomagnetic/python/fields.py --in-dir "$out_dir" --out-dir "$out_dir" --verbose True --threads 8
+    python3 gravitomagnetic/python/powerspec.py --in-dir "$out_dir" --out-dir "$out_dir" --verbose True --threads 8
+
+done
