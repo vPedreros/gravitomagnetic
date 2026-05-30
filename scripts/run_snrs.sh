babb5350664839a86f2a4d008f0ac87bb1b8ac81
Vicente Pedreros Aballay <71951212+vPedreros@users.noreply.github.com>
2026-05-29 23:12:23 -0400

    fixed scripts for new data organisation

--- scripts/run_snrs.sh
+++ scripts/run_snrs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -l
 source /Users/vpedreros/nerding/gravitomagnetic/.venv/bin/activate
+export VP_PARAMS_FILE="output/lcdm/node_037/parameters-usedvalues"
 
 for z in $(seq 0.5 0.1 3); do
     printf "\rProcessing redshift: %s..." "$z"
